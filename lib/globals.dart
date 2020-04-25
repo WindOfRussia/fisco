4aed03201fe2a646b4304ad783213f4887347d7c
Edouard <tedgag@gmail.com>
2020-04-25 14:13:49 -0400

    Added basic receipt object

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,8 @@
 library fisco_app.globals;
 
+Receipt tempReceipt;
+
+var receipts = [];
 var lastWeekDataSet = [
   LastWeekCategoryVsExpense("Groceries", 100.00),
   LastWeekCategoryVsExpense("Restaurants", 50.00),
@@ -19,4 +22,19 @@ class LastWeekCategoryVsExpense {
   final double expense;
 
   LastWeekCategoryVsExpense(this.category, this.expense);
+}
+
+class Receipt {
+  String file = "";
+  String category = "Others";
+  var date = new DateTime.now();
+  double TPS = 0, TVP = 0, total = 0;
+  List<Item> items = [];
+
+  Receipt(this.file, this.category,this.date, this.TPS, this.TVP, this.total, this.items);
+
+}
+class Item {
+  String name;
+  double price;
 }
