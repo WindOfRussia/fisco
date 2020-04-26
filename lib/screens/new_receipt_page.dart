6fd216054f4ef838958566614b4360303571f7f0
Edouard <tedgag@gmail.com>
2020-04-26 03:23:48 -0400

    Fix bad import

--- lib/screens/new_receipt_page.dart
+++ lib/screens/new_receipt_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../globals.dart' as globals;
 import '../widgets/fisco_bottom_bar.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 import '../models/line_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
